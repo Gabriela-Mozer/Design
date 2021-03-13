b0eefd6012ceb84f915b1354551720fb01634831
Gabriela Mozer <gabrielamozer1966@gmail.com>
2021-03-13 18:15:35 +0100

    cos zjebanego rotate blaaaa

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-console.log('suuup?');
-// document.getElementById("buttonTimes");
-
+// console.log('suuup?');
+// // document.getElementById("buttonTimes");
+// const hamBtn = document.querySelector('.hamburger');
+// let menuOpen = false ;
+// hamBtn.addEventListener('click',() =>{
+//     if(!menuOpen){
+//  hamBtn.classList.add('open');
+//  menuOpen = true;
+//     } else{
+//         hamBtn.classList.remove('open');
+//         menuOpen = false;
+//     }
+// });
