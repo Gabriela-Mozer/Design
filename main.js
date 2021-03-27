38275c2a5633079d495b527ffbe2b3aedf24322b
unknown <gabrielamozer1966@gmail.com>
2021-03-27 11:16:53 +0100

    adding js

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-// console.log('suuup?');
-// // document.getElementById("buttonTimes");
-// const hamBtn = document.querySelector('.hamburger');
-// let menuOpen = false ;
-// hamBtn.addEventListener('click',() =>{
-//     if(!menuOpen){
-//  hamBtn.classList.add('open');
-//  menuOpen = true;
-//     } else{
-//         hamBtn.classList.remove('open');
-//         menuOpen = false;
-//     }
-// });
+function openNav(){
+    document.getElementById("side-list").style.width="250px";
+}
+function closeNav(){
+    document.getElementById("side-list").style.width="0";
+}
